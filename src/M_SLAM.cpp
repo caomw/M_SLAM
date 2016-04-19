da167bf39eaae19ac40b2bcd3ddf7eb30a7160bb
steve <551619855@qq.com>
2016-04-19 06:30:25 -0700

    simple laser odometry

--- src/M_SLAM.cpp
+++ src/M_SLAM.cpp
@@ -14,6 +14,7 @@
 
 
 
+
 ros::Publisher pub;
 
 
@@ -30,42 +31,45 @@ void transformCallback(const sensor_msgs::PointCloud2::ConstPtr& pc_msg_ptr)
     pcl::PointCloud<pcl::PointXYZ>::Ptr src_ptr(new pcl::PointCloud<pcl::PointXYZ>);
     pcl::PointCloud<pcl::Normal>::Ptr src_normal_ptr(new pcl::PointCloud<pcl::Normal>);
     pcl::fromROSMsg(*pc_msg_ptr,*src_ptr);
-    std::cout <<"1"<<std::endl;
+    //std::cout <<"1"<<std::endl;
     if(last_pc_ptr->size() < 10)
     {
         std::cout <<"2"<<std::endl;
+        c_TransformMatrix=Eigen::Matrix4f::Identity();
 
-        //*last_pc_ptr = *src_ptr;
-        last_pc_ptr->clear();
-        last_pc_ptr->resize(src_ptr->size());
-        for(int i(0);i<src_ptr->size();++i)
-        {
-            last_pc_ptr->at(i) = src_ptr->at(i);
-        }
-        //normal_estimation.setInputCloud(last_pc_ptr);
-        //normal_estimation.compute(*last_normal_ptr);
+        *last_pc_ptr = *src_ptr;
+
 
         return;
     }
     //normal_estimation.setInputCloud(src_ptr);
     //normal_estimation.compute(*src_normal_ptr);
-    std::cout <<"3"<<std::endl;
+   // std::cout <<"3"<<std::endl;
     pcl::IterativeClosestPoint<pcl::PointXYZ,pcl::PointXYZ> icp;
 
-    icp.setInputSource(last_pc_ptr);
+    icp.setInputSource(src_ptr);
     if(src_ptr->size()<10) return;
-    icp.setInputTarget(src_ptr);
+    icp.setInputTarget(last_pc_ptr);
     std::cout <<"4"<<std::endl;
 
     std::cout << src_ptr->size()<< ":"<<last_pc_ptr->size()<<std::endl;
     pcl::PointCloud<pcl::PointXYZ> tp;
+
     icp.align(tp);
     std::cout <<"5"<<std::endl;
 
-    //c_TransformMatrix = c_TransformMatrix * icp.getFinalTransformation();
+    pcl::PointCloud<pcl::PointXYZ>::Ptr tr_pc_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+
+    c_TransformMatrix =  c_TransformMatrix * icp.getFinalTransformation() ;
+    pcl::transformPointCloud(*src_ptr,*tr_pc_ptr,c_TransformMatrix);
+    *last_pc_ptr = *src_ptr;
+
 
     sensor_msgs::PointCloud2 pub_msg;
-    pcl::toROSMsg(*last_pc_ptr,pub_msg);
+    pcl::toROSMsg(*tr_pc_ptr,pub_msg);
+
+    std::cout << std::endl;
+    std::cout << c_TransformMatrix <<std::endl;
 
     pub_msg.header.stamp=pc_msg_ptr->header.stamp;
     pub_msg.header.frame_id = pc_msg_ptr->header.frame_id;
